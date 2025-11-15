2ea4953107fefa34391fd717b27f2a489ab761ce
Vetint02 <roy.park.0704@gmail.com>
2025-11-15 14:04:11 -0800

    assignment

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-const HARD_CODED_JSON = {
+/*const HARD_CODED_JSON = {
   results: [
     {
       name: {
@@ -20,19 +20,31 @@ const HARD_CODED_JSON = {
     },
   ],
 };
+*/
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Users API!");
 });
 
 app.get("/users", (req, res) => {
+  fetch('https://randomuser.me/api')
+    .then(response => response.json())
+    .then(data => {
+      res.json(data.results[0]);
+    })
+    .catch(error => {
+      console.error('Error fetching data: ', error);
+    });
+});
+
+/*app.get("/users", (req, res) => {
     // For now, we're using hardcoded JSON data
     let randomUser = HARD_CODED_JSON.results[0];
     res.json(randomUser);
 
     // TODO: Replace the hardcoded JSON with a real API request to get a random user
     // For example, you might use the `axios` library to make a GET request to the API endpoint
-  });
+  });*/
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
